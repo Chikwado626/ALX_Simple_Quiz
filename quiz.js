a1d2144faae13d1b318c71d7afd2c9ac575ba298
Chikwado626 <chikwadoezemma03@gmail.com>
2024-07-13 06:17:31 -0500

    calculator.js

--- quiz.js
+++ quiz.js
@@ -1,10 +1,13 @@
 function checkAnswer() {
-    const correctAnswer= "4"
+    const correctAnswer = "4"
 
-const userAnswer= document.querySelector('input{name="quiz"}:checked').value;
+    const userAnswer = document.querySelector('input{name="quiz"}:checked').value;
 
-if (userAnswer===correctAnswer) {
-    document.getElementById("feedback").textContent= "Correct! well done.";
+    if (userAnswer === correctAnswer) {
+        document.getElementById("feedback").textContent = "Correct, well done.";
+    } else {
+        document.getElementById("feedback").textContent = "incorrect, try again.";
+    }
 }
-
-}
+const submitButton = document.getElementById("submit-answer");
+submitButton.addEventlistener("click", checkAnswer);
