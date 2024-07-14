31d1a059e5b990b8b3a9cbef59c8771fc5a6c28d
Chikwado626 <chikwadoezemma03@gmail.com>
2024-07-14 10:13:58 +0100

    index.html

--- quiz.js
+++ quiz.js
@@ -1,13 +1,12 @@
 function checkAnswer() {
-    const correctAnswer = "4"
+    let correctAnswer = "4"
 
-    const userAnswer = document.querySelector('input{name="quiz"}:checked').value;
+    let userAnswer = document.querySelector("input[name=quiz]:checked").value;
 
     if (userAnswer === correctAnswer) {
-        document.getElementById("feedback").textContent = "Correct, well done.";
+        document.getElementById("feedback").textContent = "Correct, well done";
     } else {
-        document.getElementById("feedback").textContent = "incorrect, try again.";
+        document.getElementById("feedback").textContent = "incorrect, try again";
     }
 }
-const submitButton = document.getElementById("submit-answer");
-submitButton.addEventlistener("click", checkAnswer);
+document.getElementById("submit-answer").addEventlistener("click", checkAnswer);
